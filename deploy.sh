ae734780425cfc8e520f6d0294e1176b357bc592
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2025-11-18 12:10:42 -0500

    Update deploy script for Docker setup

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
-# Actualiza el sistema
 sudo apt update && sudo apt upgrade -y
 
-# Instala Docker y Docker Compose
 sudo apt install -y docker.io docker-compose
 
-# (Opcional) Instala git si vas a clonar tu repo
 sudo apt install -y git
 
-# Clona el repo de GitHub
 git clone https://github.com/seminarioA/findParking.git
 
-# Construye y levanta los contenedores
+cd findParking
+
 sudo docker-compose up --build -d
 
-# Muestra estado de los contenedores
 sudo docker ps
